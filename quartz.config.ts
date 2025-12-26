6bba3ece0c33835437fb96cf88c5d522e947cb37
Zelmana <zerjar@gmail.com>
2025-12-26 15:38:26 -0500

    Quartz sync: Dec 26, 2025, 3:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "zelmana.github.io/garden/",
+    baseUrl: "zelmana.github.io/garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
